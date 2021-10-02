23903b10d51ed76ab416bbeb2d06d39127e3a94c
uberswe <m@rkus.io>
2021-10-02 17:46:15 +0200

    initial websocket code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
-//const socketio = require('socket.io');
+const socketio = require('socket.io');
 const pages = require('./pages');
 
 const app = express();
@@ -12,20 +12,23 @@ console.log(`Serving static from ${clientPath}`);
 app.use(express.static(clientPath));
 
 const server = http.createServer(app);
-//const io = socketio(server);
+const io = socketio(server);
 
 // Forward routing to seperate class to keep this class cleaner
 app.use('/', pages);
 
-//io.on('connection', (sock) => {
-    // TODO:
-    // After the user has logged in and selects a server
-    // Connect the user to all others on that server
-
-//    sock.on('message', (text) => {
-//        io.emit('message', text);
-//    });
-//});
+io.on('connection', (sock) => {
+    sock.on('message', (text) => {
+        // This sock.id is how we can identify clients for now but it changes if a user disconnects because they reconnect the page or something
+        console.log("message received: " + sock.id)
+        // TODO This is emitting the received message to all other clients
+        io.emit('message', text);
+    });
+    sock.on('disconnect', () => {
+        console.log('user disconnected ' + sock.id);
+    });
+    console.log('a user connected ' + sock.id);
+});
 
 server.on('error', (err) => {
     console.error('Server error:', err);
