14fdf994a29201d228d874c3944903ef7e81f0af
uberswe <m@rkus.io>
2021-10-04 01:47:02 +0200

    Added bot spawning

--- client/src/bot.js
+++ client/src/bot.js
@@ -1,26 +1,38 @@
 import { coordToPos } from "./util";
 
-class Bot {
-    ctx;
-    id;
-    coord;
+export class Bot {
+    ctx
+    id
+    coord
+    tileSize
 
     constructor(id, coord, ctx, tileSize){
         this.id = id;
         this.coord = coord;
         this.ctx = ctx;
         this.tileSize = tileSize
-        this.draw(coordToPos(coord[0], coord[1], this.tileSize), coordToPos(coord[0], coord[1], this.tileSize));
+        this.draw(coordToPos(coord[0], coord[1], this.tileSize), this.tileSize[0], this.tileSize[1]);
     }
 
-    draw(x, y){
-        // Green Trapazoid
+    draw(coords, w, h){
+        let x = coords[0]
+        let y = coords[1]
+        // Blue Trapazoid
+        w = w - w/3
+        h = h - h/3
         this.ctx.beginPath();
-
-
-
-        this.ctx.closePath();
+        this.ctx.moveTo((x - w/2) + (w/8), y - h/2);
+        this.ctx.lineTo((x + w/2) - (w/8), y - h/2);
+        this.ctx.lineTo(x + w/2, y + h/2);
+        this.ctx.lineTo(x - w/2, y + h/2);
+        this.ctx.lineTo((x - w/2) + (w/8), y - h/2);
+        // Navy blue
+        this.ctx.lineWidth = 1
+        this.ctx.fillStyle = "#000080"
+        this.ctx.fill();
+        this.ctx.strokeStyle = "#000080";
         this.ctx.stroke();
+        this.ctx.closePath();
     }
 
     move(x, y){
@@ -31,5 +43,3 @@ class Bot {
 
     }
 }
-
-module.exports = Bot;
--- client/src/tile.js
+++ client/src/tile.js
@@ -11,15 +11,23 @@ class Tile {
         this.draw(this.pos[0], this.pos[1], this.size[0], this.size[1]);
     }
 
+    // Each tile only needs to be two lines as the other squares will draw the other edges
     draw(x, y, w, h){
         // Square tiles
         this.ctx.beginPath();
+
+        // This sets the starting position
         this.ctx.moveTo(x - w/2, y - h/2);
+
+        // This draws the first line
         this.ctx.lineTo(x + w/2, y - h/2);
+
+        // This draws the second line
         this.ctx.lineTo(x + w/2, y + h/2);
-        this.ctx.lineTo(x - w/2, y + h/2);
-        this.ctx.closePath();
         this.ctx.stroke();
+
+        // Finish path
+        this.ctx.closePath();
     }
 }
 
