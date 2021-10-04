7969ca2d58b0c0b3cca76c04af2d3c00d905ca3c
Markus Tenghamn <markus@tenghamn.com>
2021-10-04 02:41:13 +0200

    Added score functionality (#29)

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import { Client } from './client'
 import { Canvas } from './canvas'
 import { Turn } from './turn'
 import css from './css/main.css'
+import {Scores} from "./scores";
 
 let client = new Client()
 let canvas = new Canvas()
 let turn = new Turn()
+let scores = new Scores()
 
 client.stateListener(canvas.update)
 client.stateListener(turn.update)
+client.stateListener(scores.update)
 
 const deployBot = document.getElementById('deployBot');
 deployBot.addEventListener('click', function (e) {
