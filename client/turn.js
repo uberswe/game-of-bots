2e7c8c7bd33f5576832f7a60bbed7e47517f0d01
Peter Tenghamn <petertenghamn@hotmail.com>
2021-11-13 16:46:08 +0100

    Countdown Displayed

--- client/turn.js
+++ client/turn.js
@@ -1,6 +1,7 @@
 export class Turn {
     turnElement
     timeRemaining
+    countdown
 
     constructor() {
         this.update = this.update.bind(this)
@@ -11,8 +12,19 @@ export class Turn {
         this.turnElement.innerHTML = "<h3>Time remaining " + this.timeRemaining + "</h3>"
     }
 
+    drawCountDown(){
+        this.turnElement.innerHTML = "<h3>Game Starts in " + this.countdown + "</h3>"
+    }
+
     update(obj) {
         this.timeRemaining = obj.timeRemaining
-        this.draw()
+        this.countdown = obj.countdown
+
+        if (this.countdown > 0){
+            this.drawCountDown()
+        }
+        else {
+            this.draw()
+        }
     }
 }
